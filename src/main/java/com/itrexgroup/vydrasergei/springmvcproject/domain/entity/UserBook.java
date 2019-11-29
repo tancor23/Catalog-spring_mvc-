ae33b0f431ad16272b019c8d0b6dca9f4ceaedb6
Sergei Vydra <sergei.vydra@itrexgroup.com>
2019-11-29 12:01:51 +0300

    added UserBook domain table config

--- src/main/java/com/itrexgroup/vydrasergei/springmvcproject/domain/entity/UserBook.java
+++ src/main/java/com/itrexgroup/vydrasergei/springmvcproject/domain/entity/UserBook.java
@@ -2,10 +2,7 @@ package com.itrexgroup.vydrasergei.springmvcproject.domain.entity;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -14,14 +11,19 @@ import javax.persistence.Table;
 @EqualsAndHashCode
 @ToString
 @Entity
-@Table(name = "user_book", schema = "catalog_mysql")
+@Table(name = "user_book", schema = "catalog_mysql",  uniqueConstraints=
+@UniqueConstraint(columnNames={"user_id", "book_id"}))
 public class UserBook {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private long id = -1;
 
-    @Column(name = "user_id")
+    @Column(name = "user_id", nullable = false)
     private Long userId;
 
-    @Column(name = "book_id")
+    @Column(name = "book_id", nullable = false)
     private Long bookId;
 }
 
